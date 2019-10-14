a5ea4929325c42998a197b44811b8a2b022c8295
gordon <gordon@blackmesanetwork.com>
2019-10-13 22:41:05 -0700

    tr3b

--- stringkeeper/settings.py
+++ stringkeeper/settings.py
@@ -147,7 +147,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ # components
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
