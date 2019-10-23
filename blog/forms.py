3025169a54a3998eb707a696d37348208b5cef9f
gordon <gordon@blackmesanetwork.com>
2019-10-22 23:20:56 -0700

    tr3b

--- blog/forms.py
+++ blog/forms.py
@@ -17,11 +17,20 @@ class BlogPostModelForm(forms.ModelForm):
 
     #taking args and keyword args 
     def clean_title(self, *args, **kwargs):
+        #how to show all of the components of this object - for listing things like instance
+        print(str(str('print(dir(self)): ') + str(dir(self))))
+        instance = self.instance
+        print(str('instance: ') + str(instance)) # if instance is none - that means its a new blog post
         title = self.cleaned_data.get('title')
         #here's a way we can check for duplicate titles
         #iexact means it doesnt matter if it's using upper or lowercase
         # see https://docs.djangoproject.com/en/2.2/ref/models/querysets/
         qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            # so if we are updating a blog and not creating one...
+            # so take a query set that matches this title, or whatever
+            # then we don't want the validation error on the instance we are changing
+            qs = qs.exclude(pk=instance.pk) # id = instance.id
         if qs.exists():
             raise forms.ValidationError("This title has already been used.")
         print(title)
