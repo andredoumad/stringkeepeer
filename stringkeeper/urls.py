75776d8a8d88eecfd08dc61bc9ee546c7332909b
Andre Doumad <andredoumad@gmail.com>
2019-10-29 12:29:19 -0700

    gman

--- stringkeeper/urls.py
+++ stringkeeper/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
+#if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
