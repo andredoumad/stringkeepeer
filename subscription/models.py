65c5c3e118799dfd48862225057309adff0172be
Andre Doumad <andredoumad@gmail.com>
2019-11-03 21:02:09 -0800

    gman

--- subscription/models.py
+++ subscription/models.py
@@ -5,6 +5,7 @@ class Subscription(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(max_digits=20,  decimal_places=2)
+    image           = models.FileField(upload_to='subscription/', null=True, blank=True)
 
     #this will show the overriding the 'class name' by the title string
     def __str__(self):
