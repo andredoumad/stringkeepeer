54010af3d69dd31cf33cb437dbd6a42bf7e4cbeb
andre <andre@stringkeeper.com>
2020-02-01 12:48:02 -0800

    tr3b

--- stringkeeper/settings.py
+++ stringkeeper/settings.py
@@ -456,9 +456,8 @@ if socket.gethostname()=="www.stringkeeper.com":
 
     CHANNEL_LAYERS = {
         'default': {
-            # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
-            'BACKEND': 'asgiref.inmemory.ChannelLayer',
-            'ROUTING': 'stringkeeper.routing.channel_routing',
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            # 'BACKEND': 'asgiref.inmemory.ChannelLayer',
             'CONFIG': {
                 "hosts": [('www.stringkeeper.com', 6379)],
                 # "hosts": [('44.225.82.162', 6379)],
