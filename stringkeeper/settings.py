1b6c18f4867a17c9bba2632ca4b0406dd73324a0
andre <andre@stringkeeper.com>
2020-02-02 12:45:16 -0800

    tr3b

--- stringkeeper/settings.py
+++ stringkeeper/settings.py
@@ -9,24 +9,12 @@ import logging
 
 logging.basicConfig(level=logging.CRITICAL)
 
-# CRITICAL
-# ERROR
-# WARNING
-# INFO
-# DEBUG
-# NOTSET
-
-
-# from django.contrib.auth.models import User
-# SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 eventlog('BASE_DIR: ' + str(BASE_DIR))
 BASE_URL = ''
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '89y+eudf0eoqxck3bk4=$c5#l#b7j2i4y0!k)5dta7qu-dy3ir' comprimised
