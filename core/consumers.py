3ec8cbe7f211fdef35a2b9f8e322c5fde21eada3
andre <andre@stringkeeper.com>
2020-02-04 11:28:54 -0800

    tr3b

--- core/consumers.py
+++ core/consumers.py
@@ -12,9 +12,9 @@ def test_consumer(message):
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
         eventlog('async def connect')
         user_id = self.scope["session"]["_auth_user_id"]
         self.group_name = "{}".format(user_id)
@@ -28,9 +28,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
         eventlog('async def disconnect: ' + str(close_code))
         # Leave room group
         await self.channel_layer.group_discard(
@@ -40,9 +40,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data=None,bytes_data = None):
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
         eventlog('async def receive: ' + str(text_data))
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
@@ -56,9 +56,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def recieve_group_message(self, event):
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
-        eventlog('CONNECTED CONNECTED CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
+        eventlog('CONNECTED CORE CONNECTED !!!!')
         eventlog('async def recieve_group_message event: ' + str(event))
         message = event['message']
         eventlog('async def recieve_group_message message: ' + str(message))
