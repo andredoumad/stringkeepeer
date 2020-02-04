769f9790d6dc49d6d714c62e8f919f81293a53ef
andre <andre@stringkeeper.com>
2020-02-04 12:55:30 -0800

    tr3b

--- stringkeeper/routing.py
+++ stringkeeper/routing.py
@@ -1,7 +1,6 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from core import routing as core_routing
-from example import routing as example_routing
 
 from django.conf.urls import url
 
