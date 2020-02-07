0b60d94e8f8e5d2afc49fb51ca0c0f70012134e8
andre <andre@stringkeeper.com>
2020-02-07 13:37:10 -0800

    tr3b

--- chat/consumers.py
+++ chat/consumers.py
@@ -19,7 +19,7 @@ class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         eventlog('ChatConsumer connected, event: ' + str(event))
         await self.send({
-            "type": "websocket.accept"
+            "type": "websocket.accept",
         })
 
     async def websocket_receive(self, event):
