6deae8c406e8f62a50fd1ec1625ec20d9f6a2565
andre <andre@stringkeeper.com>
2020-02-07 21:56:40 -0800

    tr3b

--- stringkeeper/settings.py
+++ stringkeeper/settings.py
@@ -243,8 +243,8 @@ if socket.gethostname()=="www.stringkeeper.com":
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE=True
     SESSION_COOKIE_HTTPONLY=True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-    SECURE_SSL_REDIRECT = True
+    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+    # SECURE_SSL_REDIRECT = True
 
     eventlog('log filepath: ' + str(os.path.join(BASE_DIR, 'stringkeeperremotedebug.log')) )
     f = open(str(os.path.join(BASE_DIR, 'stringkeeperremotedebug.log')), "w")
