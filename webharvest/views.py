4550245df5e71333484d771900f5a8d80c3f5848
andre <andre@stringkeeper.com>
2020-03-16 19:26:02 -0700

    tr3b

--- webharvest/views.py
+++ webharvest/views.py
@@ -519,6 +519,8 @@ class WebHarvestWebhookView(CsrfExemptMixin, View): # HTTP GET -- def get() CSRF
         if 'human' in data:
             eventlog('user: ' + str(data['human']))
             User = get_user_model()    
+            human = str(data['human'])
+            eventlog('human: ' + str(human))
             user = User.objects.get(email=human)
             eventlog('post user target: ' + str(user))
             eventlog('post user id: ' + str(user.user_id))
@@ -538,7 +540,7 @@ class WebHarvestWebhookView(CsrfExemptMixin, View): # HTTP GET -- def get() CSRF
                     'username': 'Alice'
             }
             
-            thread_obj = WebharvestThread.objects.get_or_new((str(human)), 'Alice')[0]
+            thread_obj = WebharvestThread.objects.get_or_new(human, 'Alice')[0]
             WebharvestChatMessage.objects.create(thread=thread_obj, user='Alice', message=str(data['chat_message']))
             # asyncio.get_event_loop().run_until_complete(command_receiver())
 
